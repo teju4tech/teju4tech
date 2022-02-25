1fa5ce6515cffe279f7a6e9df8096907a12705e8
teju4tech <solankitehpalsingh59@gmail.com>
2022-02-25 20:55:47 +0530

    ghddghd

---
+++ PROJECT_Tech_pal - Copy.py
@@ -0,0 +1,101 @@
+import os
+print("----------Welcome -----------\n")
+print("choose to do work\n\n")
+print ("press 1 for doing os work")
+print("press 2 for know about all the sasstion")
+print("\n\n ------------------------------")
+choice=int(input())
+
+if choice==2:
+       print("-------------------------------\n\n")
+       print("press 1 for sasstion 1")
+       print("press 2 for sasstion 2")
+       print("press 3 for sasstion 3")
+       print("press 4 for sasstion 4")
+       print("press 5 for sasstion 5")
+       print("press 6 for sasstion 6")
+       print("press 7 for sasstion 7")
+       print("press 8 for sasstion 8")
+       print("press 9 for sasstion 9")
+       print("press 10 for sasstion 10")
+       print("press 11 for sasstion 11")
+       print("press 12 for sasstion 12")
+       print("press 13 for sasstion 13")
+       print("press 14 for sasstion 14")
+       print("press 0 for exit")
+       print("\n\n-------------------------------")
+       press=int(input())
+       if press==1:
+                 print("----------------------------")
+                 print("In sasstion one we learnt about \n 1)git\n:-git is a software.\n it is a version control system\n 2)github\n:-it is an software Which provoid a cloud storage for your device\n And also provide git fesil5ity."  )
+       if press==2:
+                 print("In sasstion two some basic Linux command \n1)ls\n:- for showing all files in the system.\n2)cal\n:- for showing calendar\n3)date\n:- for showing date command.\n4)man\n:- for showing ability of an command.\n5)---help\n:- for help")
+       if press==3:
+                 print("In this Sasstion we learnt about\n one command is made by deep research\n like cal command \n  In the 1752 calendar shows the\n adjust days missing\n beacause This month was the offcial (British) adoption  of the geogorian \n calendar.")
+       if press==4:
+                 print("In this Sasstion In this Sasstion _ We learnt about_ Some important Linux command & What is the main difference between Program & process_ ")
+       if press==5:
+                 print("Today's session was wonderful....\n I learnt a lot by this sasstion...\n This sasstion give me a new experience...\n ..in this this sasstion we descused.. How we make a website using A small HTML code..\n...in Linux O$ ..... Linux OS is better than Windows..\n Because it's more secure than Windows.. \n& We can easily make a website ..\n By without Purchase a domain name ...\n .....user can easily visit our website..\n With IP addresse ......") 
+       if press==6:
+                 print("In this Sasstion we learnt about some Linux command's\n1)echo\n:- for print any statement\n2)man echo\n:-menually\n3fg\n:-foregroung \n 4)sleep\n:-for  stop for 1 second.")
+       if press==7:
+                 print(" This sasstion we learn some new  Linux command's\n1)echo\n:- for print any statement\n2)man echo\n:-menually\n3fg\n:-foregroung \n 4)sleep\n:-for  stop for 1 second.")
+       if press==8:
+                 print("That was a great sasstion  learnt about some Linux command's\n1)echo\n:- for print any statement\n2)man echo\n:-menually\n3fg\n:-foregroung \n 4)sleep\n:-for  stop for 1 second.")
+       if press==9:
+                 print("This sasstion was so important learnt about some Linux command's\n1)echo\n:- for print any statement\n2)man echo\n:-menual itly\n3fg\n:-foregroung \n 4)sleep\n:-for  stop for 1 second.")
+       if press==10:
+                 print("sasstion 10th in this Sasstion we learnt about some Linux command's\n1)echo\n:- for print any statement\n2)man echo\n:-menually\n3fg\n:-foregroung \n 4)sleep\n:-for  stop for 1 second.")
+       if press==11:
+                 print("11 Sasstion & this sasstion learnt about some Linux command's\n1)echo\n:- for print any statement\n2)man echo\n:-menually\n3fg\n:-foregroung \n 4)sleep\n:-for  stop for 1 second.")
+       if press==12:
+                 print("Sasstion 12  about some Linux command's\n1)echo\n:- for print any statement\n2)man echo\n:-menually\n3fg\n:-foregroung \n 4)sleep\n:-for  stop for 1 second.")
+       if press==13:
+                 print("That sasstion is Excellent about some Linux command's\n1)echo\n:- for print any statement\n2)man echo\n:-menually\n3fg\n:-foregroung \n 4)sleep\n:-for  stop for 1 second.")
+       if press==14:
+                 print(".  This is our 14th sasstion_& \n6th cloud sasstion_ \nIn this sasstion we learnt about_\n 1) storage \n:- store the Data parmnent that are called storage\n 2) Types of Storage\n :-. i) Object storage \n:- stored image, video, audio. \nii) Block storage \n:- install os Aws \niii) File Storage \n:- This is similar like Block storage But more featured....\n BLOCK \n:- 1) Root storage\n 2)Empheral\n EBS[Elastic BLOCK storage]  ")
+       if press==0:
+           exit()
+       else :
+           print(" Wrong no Entered")
+
+
+if choice==1:
+             print("\n\n-----------------------------------------------\n\n")
+             print("press 1 for open 🔥 🦊")
+             print("press 2 for open calender")
+             print("press 3 for open time")
+             print("press 4 for open date ")
+             print("press 5 for  clear screen")
+             print("press 6 for see Backgrounds activity's")
+             print("press 7 for  see fg ")
+             print("press 8 for  run touch command")
+             print("press 9 to see list of  file")
+             print("press: 10 to see Jaadu")
+             print("press:0 for exit")
+             
+             print("\n\n---------------------------------------------------")
+             some=int(input("Enter no"))
+             if some==1:
+                  
+                 os.system("google")
+             if some==2:
+                 os.system("cal")
+             if some==3:
+                 os.system("time")
+             if some==4:
+                 os.system("date")
+             if some==5:
+                 os.system("clear")
+             if some==6:
+                 os.system("top")
+             if some==7:
+                 os.system("fg")
+             if some==8:
+                 os.system("touch")
+             if some==9:
+                 os.system("ls")
+             if some==10:
+                 os.system("ip")
+             if some==0:
+                 os.system("exit")
